568afe4cd004274bd0a453e3566ee8d111c07ae8
BT_2N2_33 <sample@bths.edu>
2024-03-18 14:00:49 -0400

    Added coordinates on the board so that it looks more like the actual game

--- src/Board.java
+++ src/Board.java
@@ -8,37 +8,39 @@ public class Board {
 
     //sets a 10 x 10 grid of green as places to target opponent ships
     private static void setTargetBoardUI() {
-        for (int row = 0; row < 12; row++) {
-            for (int col = 0; col < 12; col++) {
-                if (row == 0 || row == 11) {
-                    System.out.print(UI.getBlackSquare());
-                } else {
-                    if (col == 0 || col == 11) {
-                        System.out.print(UI.getBlackSquare());
-                    } else {
+        System.out.println("  12345678910");
+        for (int row = 0; row < 11; row++) {
+            for (int col = 0; col < 11; col++) {
+                    if (col == 0) {
+                        System.out.print((String.valueOf((char)(row + 65)) + " "));
+                    }  else {
                         System.out.print(UI.getGreenSquare());
                     }
                 }
-            }
             System.out.println();
+            }
+        setBlackBar();
         }
-    }
 
     //sets a 10 x 10 grid of the blue sea and grey blocks as ships (to be implemented)
     private static void setPlayerBoardUI() {
-        for (int row = 0; row < 12; row++) {
-            for (int col = 0; col < 12; col++) {
-                if (row == 0 || row == 11) {
-                    System.out.print(UI.getBlackSquare());
-                } else {
-                    if (col == 0 || col == 11) {
-                        System.out.print(UI.getBlackSquare());
+        System.out.println("  12345678910");
+        for (int row = 0; row < 11; row++) {
+            for (int col = 0; col < 11; col++) {
+                    if (col == 0) {
+                        System.out.print((String.valueOf((char)(row + 65)) + " "));
                     } else {
                         System.out.print(UI.getBlueSquare());
                     }
                 }
-            }
             System.out.println();
         }
+    }
+
+    private static void setBlackBar() {
+        for (int i = 0; i < 14; i++) {
+            System.out.print(UI.getBlackSquare());
+        }
+        System.out.println();
     }
 }
